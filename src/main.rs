35e528e03441d4f091ff7df3ff0040d7c679f0b9
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-03-18 18:30:57 -0400

    implemented tower-web

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate tower_web;
 
 use diesel::prelude::*;
 use dotenv::dotenv;
-use std::env;
+use std::{env, str};
+
+use tower_web::extract::{Context, Extract, Immediate};
+use tower_web::middleware::deflate::DeflateMiddleware;
+use tower_web::util::BufStream;
+use tower_web::ServiceBuilder;
+
+use flate2::Compression;
 
 pub mod models;
 pub mod schema;
@@ -11,31 +20,73 @@ pub mod schema;
 use models::{Expense, NewExpense};
 use schema::expenses::dsl::expenses;
 
-fn main() {
-    println!("Expense Tracker");
-    dotenv().ok();
+#[derive(Clone, Debug)]
+struct ExpenseTrackerAPI;
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in env file");
-    let connection = SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+impl_web! {
+    impl ExpenseTrackerAPI {
+        #[get("/")]
+        fn home(&self) -> Result<String, ()> {
+            Ok("Expense Tracker API".to_string())
+        }
 
-    let expense = NewExpense {
-        name: "Expense 2",
-        amount: 12.20,
-    };
+        #[get("/expenses")]
+        #[content_type("json")]
+        fn get_expenses(&self, param: API_Param) -> Result<Vec<Expense>, ()> {
+            let results = expenses
+                .load::<Expense>(&param.connection)
+                .expect("Error loading expenses");
 
-    diesel::insert_into(schema::expenses::table)
-        .values(&expense)
-        .execute(&connection)
-        .expect("Error saving expense");
+            Ok(results)
+        }
 
-    let results = expenses
-        .limit(1)
-        .load::<Expense>(&connection)
-        .expect("Error loading expenses");
+        #[post("/expenses")]
+        #[content_type("json")]
+        fn post_expenses(&self, param: API_Param, body: Vec<u8>) -> Result<Expense, ()> {
+            let json_string:&str = str::from_utf8(&body).unwrap();
+            let expense: NewExpense = serde_json::from_str(json_string).unwrap();
+            diesel::insert_into(schema::expenses::table)
+                        .values(&expense)
+                        .execute(&param.connection)
+                        .expect("Error saving expense");
 
-    println!("Displaying {} expenses", results.len());
-    for expense in results {
-        println!("{}", expense.name);
+            use schema::expenses::dsl::id;
+            let inserted_expense:Expense = schema::expenses::table.order(id.desc()).first(&param.connection).unwrap();
+            Ok(inserted_expense)
+        }
     }
+}
+
+struct API_Config {}
+struct API_Param {
+    connection: diesel::SqliteConnection,
+}
+
+impl<B: BufStream> Extract<B> for API_Param {
+    type Future = Immediate<API_Param>;
+
+    fn extract(context: &Context) -> Self::Future {
+        let _config = context.config::<API_Config>().unwrap();
+
+        dotenv().ok();
+
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in env file");
+        let conn = SqliteConnection::establish(&database_url)
+            .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+
+        let param = API_Param { connection: conn };
+        Immediate::ok(param)
+    }
+}
+
+pub fn main() {
+    let addr = "127.0.0.1:8080".parse().expect("Invalid address");
+    println!("Listening on http://{}", addr);
+
+    ServiceBuilder::new()
+        .resource(ExpenseTrackerAPI)
+        .config(API_Config {})
+        .middleware(DeflateMiddleware::new(Compression::best()))
+        .run(&addr)
+        .unwrap();
 }
